48b3d0c10d2622e0d6221ace2241b239b1b248f3
Damiano Donati <damiano.donati@gmail.com>
2022-05-17 22:15:17 +0100

    update ignition.cfg

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ if [ ! -f id_rsa ]; then
 	curl -o id_rsa https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant
 fi
 
+#docker run -i --rm quay.io/coreos/butane:release --pretty --strict < ignition.yml > ignition.cfg
 packer build -var-file vars.json fedora-coreos.json
